c00426df8c07f3ad1484bf5b46b7a14743168305
Tnze <cjd001113@outlook.com>
2024-05-18 22:35:41 +0800

    try config as a14

--- groups/device-type/tablet/product.mk
+++ groups/device-type/tablet/product.mk
@@ -1,5 +1,12 @@
 PRODUCT_CHARACTERISTICS := tablet
 
 PRODUCT_COPY_FILES += \
-        {{{tablet_core_hardware_path}}}/tablet_core_hardware.xml:vendor/etc/permissions/tablet_core_hardware.xml
+        {{{tablet_core_hardware_path}}}/tablet_core_hardware.xml:vendor/etc/permissions/tv_core_hardware.xml
+
+PRODUCT_PACKAGES += \
+		    TvSettingsProviderOverlay \
+
+PRODUCT_COPY_FILES += \
+    device/google/atv/atv-component-overrides.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/atv-component-overrides.xml
+
 
